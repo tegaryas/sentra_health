c7b12fdc9eb23a57ec5c1395c66eecba2a8d61e2
Tegar Yasindra <tegar.yas26@gmail.com>
2021-08-12 12:47:26 +0800

    init

---
+++ lib/constants/styles.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+
+class Palletes {
+  static const PrimaryColor = Color(0xFF1a95b0);
+}
---
+++ lib/features/authentication/presentation/login_screen.dart
@@ -0,0 +1,231 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:sentra_health/constants/styles.dart';
+import 'package:sentra_health/features/authentication/presentation/signup_screen.dart';
+
+class LoginScreen extends StatelessWidget {
+  const LoginScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    var size = MediaQuery.of(context).size;
+    return Scaffold(
+      body: Container(
+        height: size.height,
+        width: size.width,
+        decoration: BoxDecoration(
+          color: Colors.white,
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+              height: 100,
+              width: 100,
+              alignment: Alignment.center,
+              color: Colors.amber,
+              child: Text('LOGO'),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Text(
+              'Sentra Health',
+              style: GoogleFonts.montserrat(
+                fontSize: 20,
+                fontWeight: FontWeight.w600,
+              ),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            EmailForm(),
+            SizedBox(
+              height: 20,
+            ),
+            PasswordForm(),
+            SizedBox(
+              height: 30,
+            ),
+            _lupaPasswordButton(),
+            SizedBox(
+              height: 30,
+            ),
+            LoginButton(),
+            SizedBox(
+              height: 20,
+            ),
+            Container(
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    'Tidak punya akun?',
+                    style: GoogleFonts.montserrat(
+                      fontSize: 16,
+                      fontWeight: FontWeight.w600,
+                    ),
+                  ),
+                  SizedBox(
+                    width: 10,
+                  ),
+                  GestureDetector(
+                    onTap: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => SignupScreen(),
+                        ),
+                      );
+                    },
+                    child: Container(
+                      child: Text(
+                        'Daftar',
+                        style: GoogleFonts.montserrat(
+                          fontSize: 16,
+                          fontWeight: FontWeight.w600,
+                          color: Palletes.PrimaryColor,
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  _lupaPasswordButton() {
+    return GestureDetector(
+      onTap: () {},
+      child: Container(
+        alignment: Alignment.topRight,
+        margin: EdgeInsets.only(
+          right: 20,
+        ),
+        child: Text(
+          'Lupa Password?',
+          style: GoogleFonts.montserrat(
+            fontSize: 16,
+            fontWeight: FontWeight.w600,
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class LoginButton extends StatelessWidget {
+  const LoginButton({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {},
+      child: Container(
+        alignment: Alignment.center,
+        margin: EdgeInsets.symmetric(
+          horizontal: 20,
+        ),
+        padding: EdgeInsets.symmetric(
+          vertical: 20,
+        ),
+        decoration: BoxDecoration(
+          color: Palletes.PrimaryColor,
+          borderRadius: BorderRadius.circular(10),
+        ),
+        child: Text(
+          'Login',
+          style: GoogleFonts.montserrat(
+            fontSize: 16,
+            fontWeight: FontWeight.w600,
+            color: Colors.white,
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class PasswordForm extends StatelessWidget {
+  const PasswordForm({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      alignment: Alignment.centerLeft,
+      margin: EdgeInsets.symmetric(horizontal: 20),
+      padding: EdgeInsets.symmetric(vertical: 3),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        border: Border.all(
+          color: Colors.black26,
+        ),
+        borderRadius: BorderRadius.circular(10),
+      ),
+      child: TextFormField(
+        textAlignVertical: TextAlignVertical.center,
+        decoration: InputDecoration(
+          border: UnderlineInputBorder(borderSide: BorderSide.none),
+          prefixIcon: Icon(
+            Icons.lock_outline,
+            color: Colors.black54,
+          ),
+          hintText: 'Password',
+          hintStyle: GoogleFonts.montserrat(
+            color: Colors.black38,
+            fontSize: 16,
+            fontWeight: FontWeight.w500,
+          ),
+        ),
+        obscureText: true,
+      ),
+    );
+  }
+}
+
+class EmailForm extends StatelessWidget {
+  const EmailForm({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      alignment: Alignment.centerLeft,
+      margin: EdgeInsets.symmetric(horizontal: 20),
+      padding: EdgeInsets.symmetric(vertical: 3),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        border: Border.all(
+          color: Colors.black26,
+        ),
+        borderRadius: BorderRadius.circular(10),
+      ),
+      child: TextFormField(
+        textAlignVertical: TextAlignVertical.center,
+        decoration: InputDecoration(
+          border: UnderlineInputBorder(borderSide: BorderSide.none),
+          prefixIcon: Icon(
+            Icons.email_outlined,
+            color: Colors.black54,
+          ),
+          hintText: 'Masukkan Email Anda',
+          hintStyle: GoogleFonts.montserrat(
+            color: Colors.black38,
+            fontSize: 16,
+            fontWeight: FontWeight.w500,
+          ),
+        ),
+      ),
+    );
+  }
+}
