9759f9d11285690b06edcb3889904b8fc156fb5c
Tegar Yasindra <tegar.yas26@gmail.com>
2021-08-13 17:35:41 +0800

    Ganti Warna Status Bar Hape

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:sentra_health/features/authentication/presentation/login_screen.dart';
 
 void main() {
@@ -9,7 +10,16 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setSystemUIOverlayStyle(
+      SystemUiOverlayStyle.dark.copyWith(
+          statusBarColor: Colors.white, // Color for Android
+          systemNavigationBarColor: Colors.white,
+          statusBarBrightness:
+              Brightness.dark // Dark == white status bar -- for IOS.
+          ),
+    );
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: LoginScreen(),
     );
   }
