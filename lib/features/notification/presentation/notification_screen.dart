7e6290a37f756d9811434062782a10796a1c18d8
Tegar Yasindra <tegar.yas26@gmail.com>
2021-08-15 16:18:43 +0800

    Nambah Tampilan Notifikasi dan Profile

--- lib/features/notification/presentation/notification_screen.dart
+++ lib/features/notification/presentation/notification_screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/painting.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:sentra_health/constants/styles.dart';
 
 class NotificationScreen extends StatefulWidget {
   const NotificationScreen({Key? key}) : super(key: key);
@@ -10,9 +13,104 @@ class NotificationScreen extends StatefulWidget {
 class _NotificationScreenState extends State<NotificationScreen> {
   @override
   Widget build(BuildContext context) {
+    var size = MediaQuery.of(context).size;
     return Scaffold(
-      body: Center(
-        child: Text('Notification Screen'),
+      appBar: AppBar(
+        elevation: 0,
+        backgroundColor: Colors.white,
+        leadingWidth: 0,
+        title: Text(
+          'Notifikasi',
+          style: GoogleFonts.montserrat(
+            color: Colors.black,
+          ),
+        ),
+        actions: [
+          Container(
+            alignment: Alignment.centerRight,
+            margin: EdgeInsets.only(
+              right: 20,
+            ),
+            child: Text(
+              'Tandai Semua telah dibaca',
+              style: GoogleFonts.montserrat(
+                fontSize: 12,
+                color: Colors.black,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          )
+        ],
+      ),
+      body: SafeArea(
+        child: Column(
+          children: [
+            _containerNotifikasi(size),
+            _containerNotifikasi(size),
+            _containerNotifikasi(size),
+            _containerNotifikasi(size),
+          ],
+        ),
+      ),
+    );
+  }
+
+  _containerNotifikasi(size) {
+    return Container(
+      color: Palletes.PrimaryColor.withOpacity(0.1),
+      width: size.width,
+      padding: EdgeInsets.all(20),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Row(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Text(
+                'Info',
+                style: GoogleFonts.montserrat(
+                  fontSize: 14,
+                ),
+              ),
+              Text(
+                ' • ',
+                style: GoogleFonts.montserrat(
+                  fontSize: 16,
+                ),
+              ),
+              Text(
+                '07 Agu 2021',
+                style: GoogleFonts.montserrat(
+                  fontSize: 14,
+                  color: Colors.grey,
+                ),
+              ),
+            ],
+          ),
+          SizedBox(
+            height: 10,
+          ),
+          Text(
+            'Kondisi badan anda prima!',
+            style: GoogleFonts.montserrat(
+              fontSize: 18,
+              color: Colors.black,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          SizedBox(
+            height: 5,
+          ),
+          Text(
+            'Hari ini anda tidur 8 Jam pada malam hari, dan itu baik bagi tubuh anda. Pertahankan habit anda untuk menjaga kondisi badan tetap fit!',
+            style: GoogleFonts.montserrat(
+              fontSize: 16,
+              color: Colors.black45,
+              fontWeight: FontWeight.normal,
+              height: 1.5,
+            ),
+          ),
+        ],
       ),
     );
   }
--- lib/features/profile/presentation/profile_screen.dart
+++ lib/features/profile/presentation/profile_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:sentra_health/constants/styles.dart';
 
 class ProfileScreen extends StatefulWidget {
   const ProfileScreen({Key? key}) : super(key: key);
@@ -11,8 +13,127 @@ class _ProfileScreenState extends State<ProfileScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text('Profile Screen'),
+      body: SafeArea(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Center(
+              child: Column(
+                children: [
+                  Container(
+                    margin: EdgeInsets.only(top: 30),
+                    child: CircleAvatar(
+                      radius: 40,
+                      backgroundColor: Palletes.PrimaryColor,
+                    ),
+                  ),
+                  SizedBox(
+                    height: 15,
+                  ),
+                  Text(
+                    'Tegar Yasindra',
+                    style: GoogleFonts.montserrat(
+                      fontSize: 22,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Text(
+                    'tegar.yas26@gmail.com',
+                    style: GoogleFonts.montserrat(
+                      fontSize: 18,
+                      fontWeight: FontWeight.normal,
+                    ),
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  Container(
+                    width: 150,
+                    padding: EdgeInsets.symmetric(horizontal: 10, vertical: 8),
+                    decoration: BoxDecoration(
+                      color: Palletes.PrimaryColor,
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                          'Edit Profile',
+                          style: GoogleFonts.montserrat(
+                            fontSize: 18,
+                            fontWeight: FontWeight.normal,
+                            color: Colors.white,
+                          ),
+                        ),
+                        SizedBox(
+                          width: 5,
+                        ),
+                        Icon(
+                          Icons.keyboard_arrow_right,
+                          color: Colors.white,
+                        )
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Container(
+              margin: EdgeInsets.symmetric(
+                horizontal: 10,
+              ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  ListTile(
+                    title: Text(
+                      'LOGIN',
+                      style: GoogleFonts.montserrat(
+                        fontSize: 18,
+                        color: Colors.grey,
+                        fontWeight: FontWeight.w600,
+                      ),
+                    ),
+                  ),
+                  ListTile(
+                    minLeadingWidth: 10,
+                    leading: Icon(
+                      Icons.account_box_outlined,
+                    ),
+                    title: Text(
+                      'Beralih akun',
+                      style: GoogleFonts.montserrat(
+                        fontSize: 18,
+                        color: Colors.black,
+                        fontWeight: FontWeight.w600,
+                      ),
+                    ),
+                  ),
+                  ListTile(
+                    minLeadingWidth: 10,
+                    leading: Icon(
+                      Icons.exit_to_app,
+                    ),
+                    title: Text(
+                      'Log Out',
+                      style: GoogleFonts.montserrat(
+                        fontSize: 18,
+                        color: Colors.black,
+                        fontWeight: FontWeight.w600,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
